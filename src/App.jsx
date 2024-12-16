ed196f295496cf8dfffacf75b5a20b1fbd69e96a
Léo Lourenço <leo.lsrd@gmail.com>
2024-12-16 00:23:00 -0300

    feat: estilizando o app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component } from "react";
+import "./assets/css/estilo.css";
+import cronometro from "./assets/img/cronometro.png";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <img src={cronometro} className="img" />
+        <a className="timer">0.0</a>
+        <div className="areaBtn">
+          <a className="botao">VAI</a>
+          <a className="botao">LIMPAR</a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
